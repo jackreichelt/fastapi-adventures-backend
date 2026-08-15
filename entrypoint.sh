408f23dc06e1b02af8782ee4de0af4a4ede2fe0d
Jack Reichelt <jackreichelt@gmail.com>
2026-08-15 17:30:23 +1000

    change gunicorn filenames

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 uv sync --locked --all-extras
-gunicorn --config gunicorn.conf.py src.fastapi_adventures.app:app
+gunicorn --config gunicorn.py src.fastapi_adventures.app:app
