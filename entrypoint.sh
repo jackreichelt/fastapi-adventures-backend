fcda9513f97ba64be3313e3f599591f1118ae0c0
Jack Reichelt <jackreichelt@gmail.com>
2026-08-15 17:54:54 +1000

    removing extra steps from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
-curl -LsSf https://astral.sh/uv/install.sh | sh
-uv sync --locked --all-extras
+# curl -LsSf https://astral.sh/uv/install.sh | sh
+# uv sync --locked --all-extras
 gunicorn --config gunicorn.py src.fastapi_adventures.app:app
