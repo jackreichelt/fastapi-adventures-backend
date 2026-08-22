d61d90e3b5908af0385b0585e40e3830bd3c5445
Jack Reichelt <jackreichelt@gmail.com>
2026-08-22 11:26:28 +1000

    Changed to default hub name

--- src/fastapi_adventures/routers/connections.py
+++ src/fastapi_adventures/routers/connections.py
@@ -2,7 +2,6 @@ from typing import Annotated
 
 from fastapi import APIRouter, Query
 
-from ..db import get_db
 from ..websockets.pubsub import get_pubsub_service
 
 router = APIRouter(
@@ -13,16 +12,9 @@ router = APIRouter(
 
 @router.get("/negotiate")
 async def get_slide(
-    db: get_db,
     pubsub_service: get_pubsub_service,
     mode: Annotated[str, Query()] = "audience",
 ):
-    pass
-    #     let token = await serviceClient.getClientAccessToken({roles: ["webpubsub.joinLeaveGroup", "webpubsub.sendToGroup"] });
-    # res.json({
-    #     url: token.url
-    # });
-
     token = pubsub_service.get_client_access_token(roles=[mode])
     return token
 
--- src/fastapi_adventures/websockets/pubsub.py
+++ src/fastapi_adventures/websockets/pubsub.py
@@ -5,7 +5,7 @@ from azure.messaging.webpubsubservice import WebPubSubServiceClient
 from fastapi import Depends
 
 CONNECTION_STRING = os.getenv("PUBSUB_CONNECTION_STRING", "")
-service = WebPubSubServiceClient.from_connection_string(connection_string=CONNECTION_STRING, hub="presentation")
+service = WebPubSubServiceClient.from_connection_string(connection_string=CONNECTION_STRING, hub="hub")
 
 
 async def yield_pubsub_service():
